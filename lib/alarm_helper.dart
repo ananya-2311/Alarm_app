b9c04f5aba4dfba6b09b2838299c4cd1687d86ec
Hp <ananyapawar23@gmail.com>
2021-06-21 23:46:15 +0530

    updated

---
+++ lib/alarm_helper.dart
@@ -0,0 +1,10 @@
+
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+final String tableAlarm = 'alarm';
+final String columnId = 'id';
+final String columnTitle = 'title';
+final String columnDateTime = 'alarmDateTime';
+final String columnPending = 'isPending';
+final String columnColorIndex = 'gradientColorIndex';
